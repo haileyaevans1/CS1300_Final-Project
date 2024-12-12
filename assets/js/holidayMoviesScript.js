e8c408f4998a9cc2f70e0d09ff5f638b84baea68
Rylie <rylie.isbell@atlasschool.com>
2024-12-12 15:17:14 -0600

    basis for api for the movies is added

--- assets/js/holidayMoviesScript.js
+++ assets/js/holidayMoviesScript.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('.movie-card button').forEach(button => {
-    button.addEventListener('click', async () => {
-        const ingredients = "1 cup sugar"; // Example
-        const response = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=YOUR_APP_ID&app_key=YOUR_APP_KEY&ingr=${encodeURIComponent(ingredients)}`);
-        const data = await response.json();
-        console.log(data); // Display data in the UI
-    });
-});
+db_endpoint <- "https://www.omdbapi.com/?"
+your_api_key <- "ba1e710"
+movie_title <- "t=Elf"
+omdb_api_url <- 
+  paste(db_endpoint,movie_title,your_api_key, sep ="")
+
+this_movie <- omdb_api_url
+GET()
+content(as = "text",
+encoding = "UTF-8")
+fromJSON(flatten = TRUE)
+as_tibble()
